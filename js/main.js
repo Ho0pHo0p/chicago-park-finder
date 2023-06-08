bb18d705a1aedb53dd24db6299c174d07be354f2
Hope G <119355967+Ho0pHo0p@users.noreply.github.com>
2023-06-08 18:00:40 -0400

    Input bar auto completes state

--- js/main.js
+++ js/main.js
@@ -5,13 +5,10 @@ key = 'haJKepRaRFghVDhjTDkpnQgsvc03Qs1gvXUL5PL2'
 
 const featureContainer = document.querySelector('.features-list'); 
 const featureExpand = document.querySelector('.features-expand');
-
-const stateContainer = document.querySelector('#state-list');
-
-const filterMenu = document.querySelector('.search-menu'); 
+const searchMenu = document.querySelector('.search-menu'); 
 const filterBtn = document.querySelector('.fa-filter');
-const searchBar = document.querySelector('.search-bar');
-const emblem = document.querySelector('.fa-tree')
+const searchBar = document.querySelector('#search-bar');
+const emblem = document.querySelector('.fa-tree');
 
 // Objects 
 
@@ -22,133 +19,92 @@ const activity = {
 
 const stateCodes = {
   name: [
-    'Alabama',
-    'Alaska',
-    'Arizona',
-    'Arkansas',
-    'California',
-    'Colorado',
-    'Connecticut',
-    'Delaware',
-    'Florida',
-    'Georgia',
-    'Hawaii',
-    'Idaho',
-    'Illinois',
-    'Indiana',
-    'Iowa',
-    'Kansas',
-    'Kentucky',
-    'Louisiana',
-    'Maine',
-    'Maryland',
-    'Massachusetts',
-    'Michigan',
-    'Minnesota',
-    'Mississippi',
-    'Missouri',
-    'Montana',
-    'Nebraska',
-    'Nevada',
-    'New Hampshire',
-    'New Jersey',
-    'New Mexico',
-    'New York',
-    'North Carolina',
-    'North Dakota',
-    'Ohio',
-    'Oklahoma',
-    'Oregon',
-    'Pennsylvania',
-    'Rhode Island',
-    'South Carolina',
-    'South Dakota',
-    'Tennessee',
-    'Texas',
-    'Utah',
-    'Vermont',
-    'Virginia',
-    'Washington',
-    'West Virginia',
-    'Wisconsin',
-    'Wyoming'
+    "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
   ],
   code: [
-    'AK','AL','AR','AZ','CA','CO','CT','DE','FL','GA','HI','IA','ID','IL','IN','KS','KY','LA','MA','MD','ME','MI','MN','MO','MS','MT','NC','ND','NE','NH','NJ','NM','NV','NY','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VA','VT','WA','WI','WV','WY'
+    "AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MP", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UM", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"
   ],
-  li: [
-  ]
+}
+/*--- EVENT LISTENERS ----*/
+searchBar.addEventListener("focus", ()=> {
+  searchMenu.classList.remove("hidden"); 
+})
+
+
+/*----- FUNCTIONS----*/
+
+// auto-complete
+function autoComplete() {
+  let userInput = document.getElementById("search-bar");
+  const stateContainer = document.getElementById("state-list");
+  userInput.addEventListener('input', autoCompleteMe);
+  stateContainer.addEventListener('click', selectItem);
+
+
+  function autoCompleteMe({target}){
+    let data = target.value;
+    stateContainer.innerHTML = ''
+    if (data.length){
+      let autoCompleteValues = autoCompleteCheck(data); 
+      autoCompleteValues.forEach(value => {addItem(value); });
+    }
+  }
+  
+  function autoCompleteCheck(inputValue){
+    let stateNames = stateCodes.name; 
+    let states = [...stateNames]
+    return states.filter (
+      value => value.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  }
+
+  function addItem(value){
+    let newState = document.createElement('li');
+    newState.innerText = `${value}`
+    stateContainer.append(newState)
+  }
+
+  function selectItem({ target }) {
+    if (target.tagName === 'LI') {
+      userInput.value = target.innerText; 
+      stateContainer.innerHTML = ''
+    }
+  }
 }
 
-// FUNCTIONS 
-
-//Event Listeners 
-
+//Features
 featureExpand.addEventListener('click', (e)=>{
-  
   loadActivity();
 })
 
 
-// search bar 
-searchBar.addEventListener('focusin', ()=>{
-  filterMenu.classList.remove('hidden')
-})
-searchBar.addEventListener('focusout', ()=>{
-  filterMenu.classList.add('hidden')
-})
+// NPS API
+async function loadActivity() {
 
-
-// emblem 
-
-stateAutoFill();
-// NPS API 
-
-function stateAutoFill() {
-  for (state of stateCodes.name){
-    const newState = document.createElement("li"); 
-    newState.classList.add("state");
-    newState.innerText=`${state}`;
-    stateContainer.append(newState) 
-  }
-}
-
-for (i=0; i<=49 ; i++ ){
-    stateCodes.li[i] = stateContainer.children[i];
-    stateCodes.li[i].addEventListener("click", (e) =>{
-      console.log(e.target.innerText)
-    })
-}
-
-
-
-
-
-
-
-
-const loadActivity = async ()=> {
-
-  const activityApi = `https://developer.nps.gov/api/v1/activities?&api_key=${key}`
+  const activityApi = `https://developer.nps.gov/api/v1/activities?&api_key=${key}`;
 
   axios.get(activityApi)
-    .then ((res) => {
+    .then((res) => {
       console.log('response', res);
 
-      for(let i = 0; i <res.data.data.length; i++){      
-        activity.id[i] = res.data.data[i].id; 
-        activity.name[i] = res.data.data[i].name; 
+      for (let i = 0; i < res.data.data.length; i++) {
+        activity.id[i] = res.data.data[i].id;
+        activity.name[i] = res.data.data[i].name;
 
-          const newActivity = document.createElement('li');
-          newActivity.classList.add('act-feature')
-          newActivity.innerText = `${activity.name[i]}` 
-        
-          featureContainer.append(newActivity)
-          }
+        const newActivity = document.createElement('li');
+        newActivity.classList.add('act-feature');
+        newActivity.innerText = `${activity.name[i]}`;
+
+        featureContainer.append(newActivity);
+      }
 
     })
-    .catch ((e)=> {
+    .catch((e) => {
       console.log("error", e);
     });
 
-};
+}
+
+/*---RUN FUNCTIONS---*/
+
+autoComplete();
