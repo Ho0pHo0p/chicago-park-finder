5e9c5b5641a555c2a3a53b1d4a79e9d786478407
Hope G <119355967+Ho0pHo0p@users.noreply.github.com>
2023-07-24 12:59:56 -0400

    Update renderParkDetails to append to detail cont

--- mods/renderParkDetails.js
+++ mods/renderParkDetails.js
@@ -1,8 +1,6 @@
 
 export default function(parkDetails){
   const parkDetailsCont = document.querySelector('.park-details'); 
-  let detailArray;
-  let iconArray;
 
   console.log(parkDetails)
 
@@ -18,48 +16,51 @@ export default function(parkDetails){
   parkImgElement.setAttribute('src', `${parkDetails.photo[0].url}`);
   parkImgElement.setAttribute('alt', `${parkDetails.photo[0].altText}`);
   parkDetailsCont.append(parkImgElement); 
+  renderDetailInfo(parkDetailsCont, parkDetails)
+}
 
-  switch(parkDetails.description){
-    case parkDetails.description: 
-    createDetailName('Description', parkDetailsCont);
-    if(parkDetails.description){
-    createDetailParagrah('Description', parkDetails.description, parkDetailsCont)
-    }
-    case '':
-    console.log('huh')
-    break
-  }
 
-    createDetailName('Contact', parkDetailsCont);
-    if(parkDetails.contact){
+function renderDetailInfo(parkDetailsCont, parkDetails){
+  let detailArray;
+  let iconArray;
+  let message;
+
+  createDetailName('description','description', parkDetailsCont);
+  createDetailParagrah('description', parkDetails.description, parkDetailsCont)
+
+  createDetailName('contact', 'contact', parkDetailsCont);
     detailArray = [parkDetails.contact.phoneNum, parkDetails.contact.emailAddress];
     iconArray = ['<i class="fa-solid fa-mobile-screen-button phone"></i>', '<i class="fa-solid fa-envelope email"></i>' ]
     createDetailList('contact', detailArray, parkDetailsCont, iconArray); 
-    }
-    createDetailName('Address & Directions', parkDetailsCont);
-    if(parkDetails.address){
+
+    createDetailName('address & directions','addressDirections', parkDetailsCont);
     detailArray = [`${parkDetails.address.line1}, ${parkDetails.address.city} ${parkDetails.address.stateCode} ${parkDetails.address.postalCode}`, parkDetails.directions];
     createDetailList('addressDirections', detailArray, parkDetailsCont)
-    }
-    createDetailName('Operating Hours', parkDetailsCont);
-    if(parkDetails.hours){
+
+    createDetailName('operating hours', 'operatingHours', parkDetailsCont);
     createDetailParagrah('operatingHours', parkDetails.hours, parkDetailsCont)
-    }
-    createDetailName('Weather', parkDetailsCont);
+
+    createDetailName('weather', 'weather', parkDetailsCont);
     if(parkDetails.weather){
-    createDetailParagrah('weather', parkDetails.weather, parkDetailsCont)
+    createDetailParagrah('weather', parkDetails.weather, parkDetailsCont); 
+    } else {
+      message = 'We are sorry! There is no information about the weather for this park'
+      createDefault(message)
     }
-  switch(parkDetails.fees){
-    case [parkDetails.fees]:
-    createDetailName('Fees', parkDetailsCont);
-    createDetailParagrah('fees', parkDetails.fees, parkDetailsCont );
-  }
+
+    createDetailName('fees', 'fees', parkDetailsCont);
+    if(!parkDetails.fees[0]){
+      noInfo(`There are no fees at this park`, 'fees')
+    } else {
+      createDetailParagrah('fees', parkDetails.fees, 'fees');
+    }
+
 }
 
-function createDetailName(title, parkDetailsCont){
+function createDetailName(title, id, parkDetailsCont){
   const detailsCont = document.createElement('section');
   detailsCont.classList.add('detail');
-  detailsCont.setAttribute('id', `${title}`);
+  detailsCont.setAttribute('id', `${id}`);
   const detailNameCont = document.createElement('div');
   detailNameCont.classList.add('detail-name'); 
   const detailName = document.createElement('p');
@@ -72,27 +73,41 @@ function createDetailName(title, parkDetailsCont){
   parkDetailsCont.append(detailsCont)
 }
 
-function createDetailParagrah(title, key, parkDetailsCont){
+function createDetailParagrah(id, key, parkDetailsCont){
+  const detailCont = document.getElementById(`${id}`);
   const paragraphElement = document.createElement('p');
-  paragraphElement.setAttribute('id', `${title}-info`)
+  paragraphElement.setAttribute('id', `${id}-info`)
+  paragraphElement.classList.add('detail-info')
   paragraphElement.innerText = `${key}`;
-  parkDetailsCont.append(paragraphElement)
+  if(paragraphElement.innerText === ''){
+    noInfo(`We are sorry there is no information available about the ${id} at this park`, id)
+  }
+  detailCont.append(paragraphElement)
 }
 
-function createDetailList(title, liArray, parkDetailsCont, iconArray) {
+function createDetailList(id, liArray, parkDetailsCont, iconArray) {
+  const detailCont = document.getElementById(`${id}`);
   const ulElement = document.createElement('ul');
-  ulElement.classList.add(`${title}-ul`); 
-  parkDetailsCont.append(ulElement)
+  ulElement.classList.add(`${id}-ul`, 'detail-info');
+  detailCont.append(ulElement);
 
   for(let li of liArray){
     const liElement = document.createElement('li');
     if(iconArray){
-      const liHTML = `<li class="${title}-li">${iconArray[liArray.indexOf(li)]} ${li}</li>`
+      const liHTML = `<li class="${id}-li">${iconArray[liArray.indexOf(li)]} ${li}</li>`
       liElement.innerHTML = liHTML
     } else {
-      liElement.classList.add(`${title}-li`);
+      liElement.classList.add(`${id}-li`);
       liElement.innerText = `${li}`
     }
     ulElement.append(liElement)
   } 
+}
+
+function noInfo(message, id){
+  const detailCont = document.getElementById(`${id}`);
+  const messageElement = document.createElement('p');
+  messageElement.classList.add(`detail-info`);
+  messageElement.innerText = `${message}`
+  detailCont.append(messageElement); 
 }
